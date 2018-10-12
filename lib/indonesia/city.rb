75fb1e0f5fe807680ee4aebbb1347366662c270d
Cecep Barkah <maceonrails@gmail.com>
2018-10-12 11:00:33 +0700

    Update city.rb

--- lib/indonesia/city.rb
+++ lib/indonesia/city.rb
@@ -1,1 +1,14 @@
+module Indonesia
+  def self.cities(province_id = nil)
+    @_cities ||= begin
+      data = CSV.read('lib/data/regencies.csv')
+      data.map { |r| { id: r[0].to_i, province_id: r[1].to_i, name: r[2] } }
+    end
 
+    if province_id.nil?
+      @_cities
+    else
+      @_cities.select { |r| r[:province_id] == province_id.to_i }
+    end
+  end
+end
