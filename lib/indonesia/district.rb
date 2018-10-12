d97f966d5f18aafa03fb39a1de1ac2fac1554587
Cecep Barkah <maceonrails@gmail.com>
2018-10-12 10:48:28 +0700

    Update district.rb

--- lib/indonesia/district.rb
+++ lib/indonesia/district.rb
@@ -1,14 +1,14 @@
 module Indonesia
-  def self.districts(regency_id = nil)
+  def self.districts(city_id = nil)
     @_districts ||= begin
       data = CSV.read('lib/data/districts.csv')
-      data.map { |r| { id: r[0].to_i, regency_id: r[1].to_i, name: r[2] } }
+      data.map { |r| { id: r[0].to_i, city_id: r[1].to_i, name: r[2] } }
     end
 
-    if regency_id.nil?
+    if city_id.nil?
       @_districts
     else
-      @_districts.select { |d| d[:regency_id] == regency_id.to_i }
+      @_districts.select { |d| d[:city_id] == city_id.to_i }
     end
   end
 end
