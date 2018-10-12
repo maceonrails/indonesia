a34726d1768aa0a2f778158782ac0ada93c1d229
Cecep Barkah <maceonrails@gmail.com>
2018-10-12 11:06:34 +0700

    Update province.rb

--- lib/indonesia/province.rb
+++ lib/indonesia/province.rb
@@ -1,8 +1,8 @@
 module Indonesia
-  def self.provinces
+  def self.provinces(country_id = nil)
     @_provinces ||= begin
       data = CSV.read('lib/data/provinces.csv')
-      data.map { |r| { id: r[0].to_i, name: r[1] } }
+      data.map { |r| { id: r[0].to_i, name: r[1], country_id: country_id } }
     end
   end
 end
